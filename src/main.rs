d60e26497c9863720b68c71cf096869442afc922
Mattia <mp.web3@gmail.com>
2025-03-29 17:53:17 +0100

    refactored in lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,5 @@
-use std::{env, error::Error, fs, process};
-
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query: String = args[1].clone();
-        let file_path: String = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
+use std::{env, process};
+use minigrep::{Config, run};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -39,6 +11,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error {e}");
+        process::exit(1)
+    }
 
 }
