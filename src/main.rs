0996473a334bd653684d421eddfe22a6d839ae7a
Mattia <mp.web3@gmail.com>
2025-03-29 17:06:41 +0100

    impro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::env;
+use std::fs;
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static String> {
+
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
+
+        Ok(
+            Config { query, file_path }
+        )
+    }
+}
+
+// fn parse_config(args: &[String]) -> Config {
+//     let query: String = args[1].clone();
+//     let file_path: String = args[2].clone();
+
+//     Config { query, file_path }
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+    // let config: Config = parse_config(&args);
+    
+    let config: Config = Config::build(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
