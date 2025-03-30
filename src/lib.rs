d1e9c78f493325d37096bca23463fe4f5fdb482b
Mattia <mp.web3@gmail.com>
2025-03-30 09:36:43 +0200

    TDD

--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,42 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
-
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    
+
     println!("With text:\n{contents}");
 
     Ok(())
+}
+
+// We’ll test-drive the implementation of the functionality that will actually do the searching for
+// the query string in the file contents and produce a list of lines that match the query.
+// We’ll add this functionality in a function called search.
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
